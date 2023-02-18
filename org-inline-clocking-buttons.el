4dcda9e872695f5c9b44ce58ed2129b00aa06ee0
ParetoOptimalDev <pareto.optimal@mailfence.com>
2023-02-18 13:12:15 -0600

    package-lint changes

--- org-inline-clocking-buttons.el
+++ org-inline-clocking-buttons.el
@@ -4,7 +4,7 @@
 
 ;; Author: ParetoOptimalDev <pareto.optimal@mailfence.com>
 ;; Version: 0.1
-;; Package-Requires: ((emacs "25.1"))
+;; Package-Requires: ((emacs "26.1"))
 ;; Keywords: outlines, hypermedia, clocking, calendar
 ;; URL: https://github.com/ParetoOptimalDev/org-inline-clocking-buttons
 ;;; Commentary:
@@ -12,33 +12,37 @@
 ;; This package provides a minor mode to add buttons to an org heading inline  for clocking in and out
 
 ;;; Code:
-(defface org-inline-clock-button-face
+(defface org-inline-clocking-buttons-clock-button-face
   '((t (:inherit custom-button)))
   "Face for GFM checkboxes.")
 
 (define-button-type 'org-clock-in-button
   'follow-link t
-  'face 'org-inline-clock-button-face
+  'face 'org-inline-clocking-buttons-clock-button-face
   'mouse-face 'custom-button-mouse
   'action (lambda (b) (org-clock-in)))
 
 (define-button-type 'org-clock-out-button
   'follow-link t
-  'face 'org-inline-clock-button-face
+  'face 'org-inline-clocking-buttons-clock-button-face
   'mouse-face 'custom-button-mouse
   'action (lambda (b) (org-clock-out)))
 
-(defun has-clock-button (str)
+(defun org-inline-clocking-buttons-has-clock-button (str)
+  "Check if the clock button string STR is in the current org heading."
   (let ((heading-string (nth 4 (org-heading-components))))
     (and heading-string (s-contains? str heading-string))))
 
-(defun has-clock-out-button ()
-  (has-clock-button "Clock Out"))
+(defun org-inline-clocking-buttons-has-clock-out-button ()
+  "Check if clock out button is on current org heading."
+  (org-inline-clocking-buttons-has-clock-button "Clock Out"))
 
-(defun has-clock-in-button ()
-  (has-clock-button "Clock In"))
+(defun org-inline-clocking-buttons-has-clock-in-button ()
+  "Check if clock in button is on current org heading."
+  (org-inline-clocking-buttons-has-clock-button "Clock In"))
 
-(defun remove-clock-buttons-current-line ()
+(defun org-inline-clocking-buttons-remove-clock-buttons-current-line ()
+  "Remove all clock buttons on the current line."
   (let ((line-start-pos (progn (beginning-of-line) (point)))
 	(line-end-pos (progn (end-of-line) (point))))
     (save-excursion
@@ -48,10 +52,11 @@
 	(replace-string " Clock In" "" nil line-start-pos line-end-pos)
 	(replace-string "    Clock Out" "" nil line-start-pos line-end-pos)))))
 
-(defun pod/add-clock-in-button-to-right-of-heading ()
+(defun org-inline-clocking-buttons-add-clock-in-button-to-right-of-heading ()
+  "Add a `Clock In` button to the right of the current org heading."
   (unless (org-at-heading-p) (org-previous-visible-heading 1))
-  (unless (has-clock-in-button)
-    (remove-clock-buttons-current-line)
+  (unless (org-inline-clocking-buttons-has-clock-in-button)
+    (org-inline-clocking-buttons-remove-clock-buttons-current-line)
     (save-excursion
       (org-end-of-line)
       (let ((end-of-line-before-insert (point)))
@@ -60,10 +65,11 @@
 	       (button-end (+ 8 button-start)))
 	  (make-button button-start button-end :type 'org-clock-in-button))))))
 
-(defun pod/add-clock-out-button-to-right-of-heading ()
+(defun org-inline-clocking-buttons-add-clock-out-button-to-right-of-heading ()
+  "Add a `Clock Out` button to the right of the current org heading."
   (unless (org-at-heading-p) (org-previous-visible-heading 1))
-  (unless (has-clock-out-button)
-    (remove-clock-buttons-current-line)
+  (unless (org-inline-clocking-buttons-has-clock-out-button)
+    (org-inline-clocking-buttons-remove-clock-buttons-current-line)
     (save-excursion
       (org-end-of-line)
       (let ((end-of-line-before-insert (point)))
@@ -72,12 +78,12 @@
 	       (button-end (+ 9 button-start)))
 	  (make-button button-start button-end :type 'org-clock-out-button))))))
 
-(defun pod/remove-org-inline-clock-button-overlays ()
+(defun org-inline-clocking-buttons-remove-org-inline-clock-button-overlays ()
   "Remove all org inline clock butotn overlays in buffer."
   (save-excursion
     (save-restriction
       (widen)
-      (remove-overlays nil nil 'face 'org-inline-clock-button-face)
+      (remove-overlays nil nil 'face 'org-inline-clocking-buttons-clock-button-face)
       ;; TODO improve this by making incorrect replacement less likely
       ;; maybe make text behind buttons more unique
       (replace-regexp "    Clock In" "" nil (point-min) (point-max))
@@ -85,7 +91,7 @@
 
 ;;;###autoload
 (define-minor-mode org-inline-clocking-buttons-mode
-  "org inline clocking buttons mode"
+  "Minor mode to add clocking buttons to org headings inline."
   :lighter nil
   (if org-inline-clocking-buttons-mode
       (progn
@@ -95,3 +101,7 @@
   (pod/remove-org-inline-clock-button-overlays)))
 
 (provide 'org-inline-clocking-buttons-mode)
+
+(provide 'org-inline-clocking-buttons)
+
+;;; org-inline-clocking-buttons.el ends here
